abe8259abc2b434240fa19aa29d7eeb509f18a83
rjhay05 <41618348+rjhay05@users.noreply.github.com>
2023-03-04 13:01:59 +0800

    stars

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import TaskBar from  './components/taskbar/TaskBar';
 import Desktop from "./components/desktop/Desktop";
+import StarsBackground from "./components/shared/animation/StarsBackground";
 
 import style from './App.module.css'
 
@@ -10,6 +11,7 @@ function App() {
   return (
 
     <main className={style[ 'main-container' ]}>
+      <StarsBackground />
       <Desktop />
       <TaskBar />
     </main>
